6d4662727d20976578151cf9edefd9b69859c890
rhayssadandara <rhayssadandara98@gmail.com>
2021-11-21 03:52:06 -0300

    adding favicon

--- src/settings/index.js
+++ src/settings/index.js
@@ -14,8 +14,7 @@ export function Settings() {
         <title>ZIGGY STARDUST</title>
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <link href="https://www.iconspng.com/uploads/bolt-icon-/bolt-icon-.png" rel="icon" type="image/x-icon" />
-        
+        <link rel="shortcut icon" href="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgZGF0YS1uYW1lPSJMYXllciAxIiBpZD0iTGF5ZXJfMSIgdmlld0JveD0iMCAwIDY0IDY0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxzdHlsZT4uY2xzLTF7ZmlsbDojZWZjYzAwO308L3N0eWxlPjwvZGVmcz48dGl0bGUvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMSIgcG9pbnRzPSIyMi44OCA5IDE4LjI3IDM1LjQ1IDI3LjQ4IDM1LjQ1IDIyLjg4IDU1IDQ0LjczIDI3LjQgMzQuMzggMjcuNCA0My41OCA5IDIyLjg4IDkiLz48L3N2Zz4=" type="image/x-icon" />        
         {AOS.init()};
       </Helmet>
       
