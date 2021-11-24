ac2e0f4ffaa4402fee5f1babf71d4726f36a72f7
rhayssadandara <rhayssadandara98@gmail.com>
2021-11-24 17:01:22 -0300

    update

--- src/components/IndexPage/header/menu.js
+++ src/components/IndexPage/header/menu.js
@@ -26,7 +26,7 @@ export function Menu() {
     return (
       <S.Menu>
         <S.LogoContainer>
-          <S.NavItem to="/#">
+          <S.NavItem to="/">
             <img src={logo.url} alt=""/>
           </S.NavItem>
         </S.LogoContainer>
