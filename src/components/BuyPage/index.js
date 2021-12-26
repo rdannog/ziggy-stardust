4f979e64841915df78942c08397a4e74cd674f17
rhayssadandara <rhayssadandara98@gmail.com>
2021-12-26 12:49:53 -0300

    adding buy page info

--- src/components/BuyPage/index.js
+++ src/components/BuyPage/index.js
@@ -5,11 +5,32 @@ import * as S from './style'
 export function BuyIndex() {
   return (
     <S.Container>
-        <p>Oi</p>
-        <p>Oi</p>
-        <p>Oi</p>
-        <p>Oi</p>
-        <p>Oi</p>
+        <S.Description>
+            <div className='info'>
+              <div className='images'>
+                <img src="https://m.media-amazon.com/images/I/A1rxQMeiz9L._AC_SL1500_.jpg" alt=""/>
+                <img src="https://cover.box3.net/newsimg/dvdmov/max1319758172-back-cover.jpg" alt=""/>
+              </div>
+              <div className='text'>
+                <h2>David Bowie - Ziggy Stardust</h2>
+                <h3>R$ 59.99</h3>
+                <p><b>The Rise and Fall of Ziggy Stardust and the Spiders from Mars</b> (muitas vezes encurtado para Ziggy Stardust) é o quinto álbum de estúdio do músico britânico David Bowie.</p>
+                <p>A obra conta a história do alter ego de Bowie, chamado Ziggy Stardust, um rock star que age como um mensageiro de seres extraterrestres. Bowie criou o personagem em Nova York, enquanto promovia Hunky Dory, e interpretou-o numa turnê por Reino Unido, Japão e América do Norte.</p>
+                <p>O álbum e seu personagem ficaram conhecidos pelas influências no glam rock e pelos seus temas, que tratavam de exploração sexual e de visão de vida. Esses fatores, ao lado das ambiguidades que cercavam a sexualidade de Bowie, e corroborados pela performance inovadora de "Star Man" no Top of the Pops, levaram o álbum a ter um caráter controverso e a ser considerado uma obra seminal. O conceito do álbum explora os campos da artificialidade do rock em geral, discutindo questões políticas, sobre uso de drogas e sobre orientação sexual. O álbum foi por várias vezes considerado um dos maiores álbuns de todos os tempos, sendo que a revista Rolling Stone considerou-o o 35° melhor. Foi classificado como o vigésimeo melhor por uma pesquisa britânica, em 1997, o 24° melhor pela revista Q, um dos 100 melhores lançamentos pela revista Time e aclamado pela revista Melody Maker como o melhor disco dos anos 70. </p>
+                <p>O som do disco é metálico, melodramático, cheio de pianos grandiosos – quase cinematográficos e guitarras distorcidas que representam as ruínas do mundo que Ziggy encontra pela frente. A grandiosidade musical em si não era novidade para Bowie, mas aqui foi a primeira vez que a execução refletiu com fidelidade e justiça o alcance de suas ideias incríveis durante um álbum inteiro. Musicalmente falando, Ziggy Stardust and The Spiders From Mars é um daqueles discos visionários que continua se revelando a cada nova audição. É sexy, é dramático, é violento, é divertido, é provocador, é entorpecente. Foi um dos momentos fundamentais na obra de Bowie, consagrando criador e criatura e mudando para sempre sua importância na cultura pop.
+                </p>
+                <p className='description'>CATEGORIAS: CDs, Heavy / Hard / Gothic / Rock</p>
+                <p>TEMPO TOTAL: 38 minutos e 32 segundos</p>
+                <p>LANÇAMENTO: 6 de junho de 1972</p>
+                <div className='form'>
+                  <h2>Inscreva-se para comprar:</h2>
+                  <input type="email" placeholder="Digite seu email"/>
+                  <button>Inscrever-se</button>
+                </div>
+              </div>
+            </div>
+        </S.Description>
+        {/* <p>David Bowie: vocais, guitarra e saxofone Mick (Ronno) Ronson: guitarra, piano e vocais Mick (Woody) Woodmansey: bateria Trevor Bolder: baixo</p> */}
     </S.Container>
   )
 }
--- src/components/BuyPage/style.js
+++ src/components/BuyPage/style.js
@@ -3,3 +3,88 @@ import styled from "styled-components";
 export const Container = styled.section`
     margin-top:12vh;
 `
+export const Description = styled.section`
+    padding: 8vh 2vw;
+    .info{
+        width:100%;
+        display:flex;
+        justify-content:space-around;
+        align-items:start;
+    }
+    .images{
+        width: 45%;
+        img{
+            width:100%;
+            margin-bottom:2vh;
+        }
+    }
+    .text{
+        width:50%;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:start;
+    }
+    h1{
+        margin-bottom: 7vh;
+        font-size: 2rem;
+        text-transform: uppercase;
+        text-align: center;
+        font-family: 'Roboto', sans-serif;
+    }
+    h2{
+        margin-top: 7vh;
+        font-size: 2rem;
+        text-shadow: 0px 0px 5px #B8860C;
+    }
+    h3{
+        margin-top: 2vh;
+        margin-bottom: 3vh;
+        font-size: 1.8rem;
+        font-weight: lighter;
+        color: #5C703B;
+    }
+    p{
+        margin-bottom: 3vh;
+        text-align: justify;
+        font-size: 1.1rem;
+        font-family: 'Roboto', sans-serif;
+    }
+    b{
+        font-weight: bolder;
+        font-size: 1.2rem;
+        color:#B8860C;
+    }
+    .form{
+        width:100%;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-around;
+        align-items:center;
+        margin-top:6vh;
+    }
+    input{
+        width:70%;
+        height:7vh;
+        border-radius:12px;
+        border: 3px solid rgb(184, 134, 12);
+        padding:0.5vh 1vw;
+        margin-bottom:2vh;
+        margin-top:2vh;
+        font-size: 1rem;
+        background-color: #000;
+        cursor: text;
+    }
+    button{
+        width:50%;
+        border-radius:8px;
+        border: #000;
+        background-color: #5C7030;
+        font-size: 1.2rem;
+        padding:1.5vh 1vw;
+        cursor:pointer;
+    }
+    .description{
+        margin-top:6vh;
+    }
+`
